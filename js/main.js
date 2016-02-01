945f5be53f9d5729f9d585e2237bcaf4e24762ec
Demetra Haloutsos <demetra2h@gmail.com>
2016-02-01 09:13:53 -0800

    second

---
+++ js/main.js
@@ -0,0 +1,16 @@
+console.log( js loaded!)
+
+var tile = function(x, y) {
+  this.x = x;
+  this.y = y;
+  this.width = 70;
+};
+
+var tiles = [];
+
+for (var i = 0; i < tiles.length; i++) {
+    tiles[i].drawFaceDown();
+}
+
+
+
