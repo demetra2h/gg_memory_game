dec501ba110fb51736efbadcd444b932f4c96bab
Demetra Haloutsos <demetra2h@gmail.com>
2016-02-03 16:07:24 -0800

    implement a fisher-yates shuffle for the board

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ There are many weird, wonderful characters in the world of Lorelai and Rory Gilm
 
 Flip over the card to see who is behind it and match them. If you are correct they stay face up, if you are incorrect they flip back over and you can try again. Once all the characters have been matched, you win! 
 
-Made using jQuery for the game functionality, css for the styling and HTML.
+Made using jQuery for the game functionality, CSS for the styling and HTML.
 
 <a href="http://imgur.com/ZRlauiN"><img src="http://i.imgur.com/ZRlauiNl.jpg" title="source: imgur.com" /></a>
--- js/main.js
+++ js/main.js
@@ -1,103 +1,107 @@
-console.log(js loaded!)
+console.log('js loaded!');
 //the basic construction of the game when it's started
 
 var OpenCell = ""; //empty box
 var OpenCard = ""; //the image that is going to be placed
-var CardPulled = 0; //there hasnt been an image, or card, pulled yet
+var CardPulled = 0; //there hasnt been a card, pulled yet
 var TurnCounter = 0; //starts at zero for a new game
+//
+var Card = function(className) {
+  this.className = className;
+  this.matched = false; //because it's the start of the game
+}
 
 //this is the cards array, there are 6 being repeated twice
-  var card =
-  [
-  url("../images/emily.jpg");
-  url("../images/jess.jpg");
-  url("../images/lorelai.jpg");
-  url("../images/richard.jpg");
-  url("../images/rory.jpg");
-  url("../images/sookie.jpg");
-  url("../images/emily.jpg");
-  url("../images/jess.jpg");
-  url("../images/lorelai.jpg");
-  url("../images/richard.jpg");
-  url("../images/rory.jpg");
-  url("../images/sookie.jpg");
-  ]
+//these are the classes of the cards, the character name
+var board = [
+  new Card("emily"),
+  new Card("jess"),
+  new Card("lorelai"),
+  new Card("richard"),
+  new Card("rory"),
+  new Card("sookie"),
+  new Card("emily"),
+  new Card("jess"),
+  new Card("lorelai"),
+  new Card("richard"),
+  new Card("rory"),
+  new Card("sookie")
+];
 
 //need to put the empty cells here
-function OpenCell() {
+//they need to be filled the cards, board
+function openCell() {
   var open =
   [
-  "", "", "", "",
-  "", "", "", "",
-  "", "", "", ""
+    "cell0", "cell1", "cell2", "cell3",
+    "cell4", "cell5", "cell6", "cell7",
+    "cell8", "cell9", "cell10", "cell11"
   ]
 };
 
-//start with randomizing the image array function
-function RandomFunction(MaxValue, MinValue) {
-  return Math.round(Math.random() *
-    (MaxValue - MinValue) + MinValue);
-}
+//shuffling of the cards
+//the board array is written above
+function shuffleBoard() {
+  var currentIndex = board.length,
+      temporaryValue,
+      randomIndex;
 
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
 
-//shuffling of the cards using a for loop allImg.length is the
-//image array written above
-function shuffleArray(card) {
-  for (var i = card.length - 1; i > 0; i--) {
-    var j = Math.floor(Math.random() * (i + 1));
-    var temp = card[i];
-    card[i] = card[j];
-    card[j] = temp;
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--; //amount of cards
+
+    // And swap it with the current element.
+    temporaryValue      = board[currentIndex];
+    board[currentIndex] = board[randomIndex];
+    board[randomIndex]  = temporaryValue;
   }
-
 }
 
+//click action for the class of the cell, where the
+//cards arae going to show up
 
-//click action
-$("card").each(function(index, card) {
-  console.log( index + "; " + $( this ).text() );
+$(".card").each(function(index, card) {
+  $(this).on("click", function(){
+    //console.log('click!'); just checking if the class is working
+  });
+  // console.log( index + "; " + $( this ).text() );
 });
 
 //pulling of the cards
 function pullCard(){
-
+  $(".card").each(function(index, card) {
+    $(this).on()
+  });
 
 }
 
 
 
-//set a time interval for the cards when they are face up
+// //set a time interval for the cards when they are face up
 setTimeout(function(){
-  $(".card").remove()
-
+ $(".card").remove();
 })
 
 //callback function
-var getCard = document.getElementById('getCard');
+// var getCard = document.getElementById('getCard');
 
-getCard.addEventListener(click(function(event) {
-  alert('clicked')
-});
+// getCard.addEventListener("click", function(event) {
+//  alert('clicked')
+// });
 
-//start of the game
-game.start = function( {
+game = {};
 
-})
-
-
-//if else statement for a guess
-
-
+// //start of the game
+game.start = function() {
+};
+// });
 
 
 //turn counter??
-
-
-
 //play game button
-
-
-
 //clear board function
 
 
